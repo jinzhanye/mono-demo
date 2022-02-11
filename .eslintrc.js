9558372fbcce516c4e578591c1bf70ed4970587c
叶锦湛 <jinzhan.ye@zhenai.com>
2022-02-11 11:38:00 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
